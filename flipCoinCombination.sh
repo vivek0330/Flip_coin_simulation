f56efc427013013085e4c3064c699756a1287d7b
Vivek varshney <Vivek.iitd93@gmail.com>
2021-06-14 21:30:41 +0530

    determine the % of the singlet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,14 +1,35 @@
 #!/bin/bash -x
 
-echo "Flip coin simulation problem" 
+echo "Welcome"
 
-#INITIALIZE THE RANDOM VALUE
-random=$(( RANDOM%2 ))
+#VARIABLE INITIALIZED
+numberOfTimesHead=0
+numberOfTimesTail=0
 
-#CHECK THE CONDITION AND DISPLAY HEAD OR TAIL
-if [ $random -eq 1 ]
-then
-	echo "head"
-else
-	echo "tail"
-fi
+#TAKE USER INPUT
+read -p "Enter the limit of number of times coin flips:" limit
+
+#STORE VALUES IN THE DICTIONARY
+declare -A dict 
+for((i=1; i<=limit; i++))
+do
+	if [ $(( RANDOM%2 )) -eq 1 ]
+	then
+		dict[$i]=H
+		(( numberOfTimesHead++ ))
+	else
+		dict[$i]=T
+		(( numberOfTimesTail++ ))
+	fi
+done
+
+#DISPLAY VALUES IN THE DICTIONARY
+echo "values in the dictionary : ${dict[@]}"
+
+#CALCULATE PERCENTAGE OF HEAD AND TAIL
+headPercent=`awk 'BEGIN{printf("%0.2f" ,('$numberOfTimesHead'*100) / '$limit')}'`
+tailPercent=`awk 'BEGIN{printf("%0.2f" ,('$numberOfTimesTail'*100) / '$limit')}'`
+
+#DISPLAY PERCENTAGE OF HEAD AND TAIL
+echo "head percent = $headPercent %"
+echo "tail percent = $tailPercent %"
