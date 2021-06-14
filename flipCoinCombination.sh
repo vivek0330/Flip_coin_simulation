ac2b36512d75a2834a393511c3720e4afe7647f2
Vivek varshney <Vivek.iitd93@gmail.com>
2021-06-14 21:15:36 +0530

    Display haid and tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,4 +2,13 @@
 
 echo "Flip coin simulation problem" 
 
+#INITIALIZE THE RANDOM VALUE
+random=$(( RANDOM%2 ))
 
+#CHECK THE CONDITION AND DISPLAY HEAD OR TAIL
+if [ $random -eq 1 ]
+then
+	echo "head"
+else
+	echo "tail"
+fi
