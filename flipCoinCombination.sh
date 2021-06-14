ad61c24bada83aa10eb32ddcdd3d7dde5a03660d
Vivek varshney <Vivek.iitd93@gmail.com>
2021-06-15 00:48:31 +0530

    winner and sorting

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,39 +1,171 @@
 #!/bin/bash -x
 
-echo "Welcome"
+#CONSTANT
+COUNT=10
 
-#CONSTANT INITIALIZED
-NUMBER_OF_COINS=3
+#FUNCTION FOR FLIPPING COIN
+function flipCoin(){
+	result=$(( RANDOM%2 ))
+	echo $result
+}
 
-#TAKE USER INPUT
-read -p "Enter the limit of number of times coin flips:" limit
+#FUNCTION FOR CALCULATING PERCENTAGE
+function percentage(){
+   percent=$(( $1*10 ))
+   echo $percent
+}
 
-#STORE VALUES AND INDEX IN THE DICTIONARY
-declare -A dict 
-for((i=1; i<=limit; i++))
+#GENERATING SINGLET AND STORING IN DICTIONARY
+IS_HEAD=0
+IS_TAIL=1
+
+declare -A results
+count=0
+results[HEAD]=$count
+results[TAIL]=$count
+
+for ((  i=0 ; i<COUNT ; i++ ))
 do
-	for((j=1; j<=NUMBER_OF_COINS; j++))
+	flip=$( flipCoin )
+	if [[ flip -eq $IS_HEAD ]]
+	then
+		(( results[HEAD]++  ))
+	elif [[ flip -eq $IS_TAIL ]]
+	then
+		(( results[TAIL]++ ))
+	fi
+done
+
+#SINGLET PERCENTTAGE
+results[HEAD_Percent]=$( percentage ${results[HEAD]} )
+results[TAIL_Percent]=$( percentage ${results[TAIL]} )
+
+#GENERATING DOUBLET AND STORING IN DICITIONARY
+IS_HEAD_HEAD="00"
+IS_HEAD_TAIL="01"
+IS_TAIL_HEAD="10"
+IS_TAIL_TAIL="11"
+
+count=0
+results[HEAD_HEAD]=$count
+results[HEAD_TAIL]=$count
+results[TAIL_HEAD]=$count
+results[TAIL_TAIL]=$count
+for (( i=0 ; i<COUNT ; i++ ))
+do
+		flip=$( flipCoin )$( flipCoin )
+	if [[ flip -eq $IS_HEAD_HEAD ]]
+	then
+		(( results[HEAD_HEAD]++ ))
+	elif [[ flip -eq $IS_HEAD_TAIL ]]
+	then
+		(( results[HEAD_TAIL]++ ))
+	elif [[ flip -eq $IS_TAIL_HEAD ]]
+	then
+		(( results[TAIL_HEAD]++ ))
+	elif [[ flip -eq $IS_TAIL_TAIL ]]
+	then
+      (( results[TAIL_TAIL]++ ))
+	fi
+done
+
+#DOUBLET PERCENTAGE
+results[HEAD_HEAD_Percent]=$( percentage ${results[HEAD_HEAD]} )
+results[HEAD_TAIL_Percent]=$( percentage ${results[HEAD_TAIL]} )
+results[TAIL_HEAD_Percent]=$( percentage ${results[TAIL_HEAD]} )
+results[TAIL_TAIL_Percent]=$( percentage ${results[TAIL_TAIL]} )
+
+#GENERATING TRIPLET AND STRING IN DICTIONARY
+IS_HEAD_HEAD_HEAD="000"
+IS_HEAD_HEAD_TAIL="001"
+IS_HEAD_TAIL_HEAD="010"
+IS_TAIL_HEAD_HEAD="100"
+IS_HEAD_TAIL_TAIL="011"
+IS_TAIL_HEAD_TAIL="101"
+IS_TAIL_TAIL_HEAD="110"
+IS_TAIL_TAIL_TAIL="111"
+
+count=0
+
+results[HEAD_HEAD_HEAD]=$count
+results[HEAD_HEAD_TAIL]=$count
+results[HEAD_TAIL_HEAD]=$count
+results[TAIL_HEAD_HEAD]=$count
+results[HEAD_TAIL_TAIL]=$count
+results[TAIL_HEAD_TAIL]=$count
+results[TAIL_TAIL_HEAD]=$count
+results[TAIL_TAIL_TAIL]=$count
+
+for (( i=0 ; i<COUNT ; i++ ))
+do
+	flip=$( flipCoin )$( flipCoin )$( flipCoin )
+	if [[ flip -eq $IS_HEAD_HEAD_HEAD ]]
+	then
+		(( results[HEAD_HEAD_HEAD]++ ))
+	elif [[ flip -eq $IS_HEAD_HEAD_TAIL ]]
+	then
+		(( results[HEAD_HEAD_TAIL]++ ))
+	elif [[ flip -eq $IS_HEAD_TAIL_HEAD ]]
+	then
+		(( results[HEAD_TAIL_HEAD]++ ))
+	elif [[ flip -eq $IS_TAIL_HEAD_HEAD ]]
+	then
+		(( results[TAIL_HEAD_HEAD]++ ))
+	elif [[ flip -eq $IS_HEAD_TAIL_TAIL ]]
+	then
+		(( results[HEAD_TAIL_TAIL]++ ))
+	elif [[ flip -eq $IS_TAIL_HEAD_TAIL ]]
+	then
+		(( results[TAIL_HEAD_TAIL]++ ))
+	elif [[ flip -eq $IS_TAIL_TAIL_HEAD ]]
+	then
+		(( results[TAIL_TAIL_HEAD]++ ))
+	elif [[ flip -eq $IS_TAIL_TAIL_TAIL ]]
+	then
+		(( results[TAIL_TAIL_TAIL]++ ))
+	fi
+done
+
+#TRIPLET PERCENTAGE
+results[HEAD_HEAD_HEAD_Percent]=$( percentage ${results[HEAD_HEAD_HEAD]} )
+results[HEAD_HEAD_TAIL_Percent]=$( percentage ${results[HEAD_HEAD_TAIL]} )
+results[HEAD_TAIL_HEAD_Percent]=$( percentage ${results[HEAD_TAIL_HEAD]} )
+results[TAIL_HEAD_HEAD_Percent]=$( percentage ${results[TAIL_HEAD_HEAD]} )
+results[HEAD_TAIL_TAIL_Percent]=$( percentage ${results[HEAD_TAIL_TAIL]} )
+results[TAIL_HEAD_TAIL_Percent]=$( percentage ${results[TAIL_HEAD_TAIL]} )
+results[TAIL_TAIL_HEAD_Percent]=$( percentage ${results[TAIL_TAIL_HEAD]} )
+results[TAIL_TAIL_TAIL_Percent]=$( percentage ${results[TAIL_TAIL_TAIL]} )
+
+#STORING DICTIONARY TO ARRAY
+index=0
+for i in ${!results[@]}
+do
+	array[ (( index++ )) ]=${results[$i]}
+done
+
+#SORTING ARRAY
+length=${#array[@]}
+temporary=0
+for (( i=0 ; i<$length ; i++ ))
+do
+	for (( j=0 ; j<$length ; j++ ))
 	do
-		random=$(( RANDOM%2 ))
-		if [ $random -eq 1 ]
+		if [ ${array[i]} -lt ${array[j]} ]
 		then
-			sideOfCoin+=H
-		else
-			sideOfCoin+=T
+			temporary=${array[i]}
+			array[i]=${array[j]}
+			array[j]=$temporary
 		fi
 	done
-	(( dict[$sideOfCoin]++ ))
-	sideOfCoin=""
 done
 
-#DISPLAY VALUES AND INDEX IN THE DICTIONARY
-echo "values in the dictionary : ${dict[@]}"
-echo "indexes in the dictionary : ${!dict[@]}"
-
-#DISPLAY PERCENTAGE OF COMBINATIONS IN DICTIONARY
-echo "Percentage of head-tail combinations in the dictionary :"
-for key in ${!dict[@]}
+#FINDING WINNING COMBINATION
+highestPercent=${array[$length-1]}
+for i in ${!results[@]}
 do
-	percentage=`awk 'BEGIN{printf("%0.2f" ,('${dict[$key]}'*100) / '$limit')}'`
-	echo $percentage
+	if (( ${results[$i]}==$highestPercent ))
+	then
+		winningCombination=${i::-8}
+	fi
 done
+printf "$winningCombination\n"
